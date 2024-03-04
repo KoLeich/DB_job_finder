1f0dccce09296e0713e970e5200c72f7e0146aa4
KoLeich <124056082+KoLeich@users.noreply.github.com>
2024-03-04 14:36:16 +0100

    Update e1.sh

--- e1.sh
+++ e1.sh
@@ -5,3 +5,4 @@ echo $1
 curl -G -d "qli=true" -d "query=" -d "sort=pubExternalDate_tdt" -d "itemsPerPage=20" -d "pageNum=$1" -d "state=Berlin" https://db.jobs/service/search/de-de/5441588 |  grep jobId | grep title-text >Seiten/Seite$1.html
 
 
+# state is set to Berlin, if you want to place in another city or more cities, you have to change this value
